3eebb8726c224d97ce268a42874ed908d2c29917
Mohammed Rafiq H <120514001+Padget123@users.noreply.github.com>
2024-08-10 17:33:05 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/btwabhimanyu"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/friendshipworldgroup"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Bg6TMD1FP89lNTU1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +93,7 @@ def stream_markup(_, videoid, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/btwabhimanyu "
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/friendshipworldgroup"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Bg6TMD1FP89lNTU1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -248,7 +248,7 @@ def stream_markup2(_, chat_id):
                 text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/btwabhimanyu"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/friendshipworldgroup"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+Bg6TMD1FP89lNTU1"
             ),
         ],
 
